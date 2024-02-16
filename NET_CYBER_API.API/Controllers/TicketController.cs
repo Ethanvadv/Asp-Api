344606c632383ab17a4e4f5290708b7b741c2019
Ethanvadv <ethan.vandeven2@gmail.com>
2024-02-16 09:05:42 +0100

    Ajoutez des fichiers projet.

---
+++ NET_CYBER_API.API/Controllers/TicketController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NET_CYBER_API.DAL.Data;
+using NET_CYBER_API.Domain.Models;
+using NET_CYBER_API.BLL.Interfaces;
+
+namespace NET_CYBER_API.API.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TicketController : ControllerBase
+    {
+        private readonly ITicketService _service;
+
+        public TicketController(ITicketService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Ticket>> GetAll() 
+        { 
+            return Ok(_service.GetAll());
+        }
+
+        
+    
+    }
+}
---
+++ NET_CYBER_API.BLL/Services/TicketService.cs
@@ -0,0 +1,47 @@
+﻿using NET_CYBER_API.BLL.Interfaces;
+using NET_CYBER_API.DAL.Interfaces;
+using NET_CYBER_API.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET_CYBER_API.BLL.Services
+{
+    public class TicketService : ITicketService
+    {
+        private readonly ITicketRepository _repository;
+
+        public TicketService(ITicketRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Ticket? Create(Ticket ticket)
+        {
+            int id = _repository.GetAll().Max(t => t.Id) +1 ;
+            ticket.Id = id;
+        }
+
+        public bool Delete(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Ticket> GetAll()
+        {
+            return _repository.GetAll();
+        }
+
+        public Ticket? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Ticket? Update(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
